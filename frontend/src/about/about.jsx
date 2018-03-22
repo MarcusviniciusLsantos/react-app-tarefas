f80bfc383bf50d01eab4b00fbd90453ab2932b44
MarcusviniciusLsantos <marcusvinici25@gmail.com>
2018-03-22 11:59:48 -0300

    commitão

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Alguém' p='Chamado React'></PageHeader>
+
+        <h2>React</h2>
+        <p> É um framework voltado a front-end</p>
+        <h2>Criado por quem?</h2>
+        <p>Pelo FaciBruk</p>
+    </div>
+)
